e4efb0e00ea02b2dd7747b63862f3c0314c8582d
Balogh <i2i25q@hallgato.uni-neumann.hu>
2024-12-13 12:30:24 +0100

    init

---
+++ GAMF/Models/Course.cs
@@ -0,0 +1,14 @@
+﻿namespace GAMF.Models
+{
+    public class Course
+    {
+
+        public int CourseId { get; set; }
+
+        public string Title { get; set;}
+
+        public int Credits { get; set;}
+
+        public virtual ICollection<Enrollment> Enrollments { get; set; }
+    }
+}
---
+++ GAMF/Models/Student.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel;
+
+namespace GAMF.Models
+{
+    public class Student
+    {
+        public int Id { get; set; } 
+        public string LastName { get; set; }
+        public string FirstMidName { get; set; }
+
+        public DateTimeOffset EnrollmentDate { get; set; }
+
+        public virtual ICollection<Enrollment> Enrollments { get; set; }
+
+    }
+}
